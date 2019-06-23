5ce5053dafdec32be8be4e6c00ac8741081cc919
alex <camward@yandex.ru>
2019-06-23 14:28:20 +0400

    back-end. авторизация npm i bcrypt-nodejs jsonwebtoken

--- server/keys/keys.dev.js
+++ server/keys/keys.dev.js
@@ -1,3 +1,4 @@
 module.exports = {
-  MONGO_URI: `mongodb://camward:camward123@ds341847.mlab.com:41847/nuxt-blog`
+  MONGO_URI: `mongodb://camward:camward123@ds341847.mlab.com:41847/nuxt-blog`,
+  JWT: 'dev-jwt-key'
 }

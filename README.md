eef594ba08cc6b07643adbda952265fc4c0fb561
alex <camward@yandex.ru>
2019-06-30 16:44:07 +0400

    рефакторинг

--- README.md
+++ README.md
@@ -1,22 +1,7 @@
-# nuxt-blog
+# Блог
 
-> My delightful Nuxt.js project
+Склонировать проект: `https://github.com/camward/Nuxt-Blog.git`
 
-## Build Setup
+Перейти в папку проекта и установить зависимости: `npm i` или `yarn`
 
-``` bash
-# install dependencies
-$ npm install
-
-# serve with hot reload at localhost:3000
-$ npm run dev
-
-# build for production and launch server
-$ npm run build
-$ npm start
-
-# generate static project
-$ npm run generate
-```
-
-For detailed explanation on how things work, checkout [Nuxt.js docs](https://nuxtjs.org).
+Запустить проект командой `npm run dev` и перейти по адресу `http://localhost:3000/`
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,12 +1,14 @@
+const pkg = require('./package')
+
 module.exports = {
   mode: 'universal',
 
   head: {
-    title: process.env.npm_package_name || '',
+    title: pkg.name,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -30,8 +32,7 @@ module.exports = {
     '@nuxtjs/pwa'
   ],
 
-  axios: {
-  },
+  axios: {},
 
   env: {
     appName: 'SSR Blog'

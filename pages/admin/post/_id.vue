c31b8b73c47c5522be3d32ebcafd8adffee80942
alex <camward@yandex.ru>
2019-06-30 16:10:54 +0400

    фильтр отображения дат

--- pages/admin/post/_id.vue
+++ pages/admin/post/_id.vue
@@ -24,7 +24,7 @@
         <small class="mr">
           <i class="el-icon-time"></i>
           <span>
-            {{ new Date(post.date).toLocaleString() }}
+            {{ post.date | date }}
           </span>
         </small>
 
